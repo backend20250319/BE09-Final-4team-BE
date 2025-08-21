1735038f2c0ebd7c2865409f789d35e3fda93f30
hyeblee <hyeb1224@naver.com>
2025-08-21 15:10:39 +0900

    fix: communication-service에서 FtpService 빈 주입 위해 컴포넌트 스캔 유지
    - @SpringBootApplication(scanBasePackages = {...}) 어노테이션 제거 시 스타터 빈 등록 실패
    - 스타터 자동등록 사용을 시도했으나, 문제 발생으로 기존 스캔 유지

--- communication-service/src/main/java/com/hermes/communicationservice/CommunicationServiceApplication.java
+++ communication-service/src/main/java/com/hermes/communicationservice/CommunicationServiceApplication.java
@@ -3,7 +3,7 @@ package com.hermes.communicationservice;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.hermes.communicationservice", "com.hermes.ftpspringbootstarter"})
 public class CommunicationServiceApplication {
 
   public static void main(String[] args) {
