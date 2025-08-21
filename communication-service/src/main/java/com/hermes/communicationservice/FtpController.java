b923fe92219a3461cc420924f9e8220d02ee9484
hyeblee <124981825+hyeblee@users.noreply.github.com>
2025-08-21 16:45:32 +0900

    feat: 파일 업로드 스타터 구현 (#28)
    * feature: 공통 응답 클래스 생성
    
    * feat: FTP 스타터 초기 구현
    
    - 파일 업로드, 삭제, 존재 확인, 저장 URL 반환 기능 구현
    - UUID 기반 파일명 고유화 적용
    - 업로드 성공 시 저장된 파일명만 반환
    - FTP 스타터 사용 시 application-ftp.yml 추가 필요
    - 사용하는 서비스에서 @Autowired로 FtpService 사용 가능
    - ApiResponse는 추후 라이브러리 구현 시 수정 후 삭제 예정
    
    * feat: 커뮤니케이션 서비스 초기 설정 및 FTP API 구현
    
    - FTP 관련 컨트롤러 구현
    - 정상 동작을 위해 application-communication-db.yml, application-ftp.yml 필요
    - FtpController에서 실패 시 예외 처리 로직 추가 필요
    
    * refactor: 불필요한 scanBasePackages 제거 및 스타터 모듈 설정 추가
    
    - ftp-spring-boot-starter는 자동 빈 등록이 되므로 CommunicationServiceApplication에서 scanBasePackages 설정을 제거
    - settings.gradle에 communication-service와 ftp-spring-boot-starter 모듈 포함 설정
    
    * fix: communication-service에서 FtpService 빈 주입 위해 컴포넌트 스캔 유지
    
    - @SpringBootApplication(scanBasePackages = {...}) 어노테이션 제거 시 스타터 빈 등록 실패
    - 스타터 자동등록 사용을 시도했으나, 문제 발생으로 기존 스캔 유지
    
    * chore: settings.gradle에 communication-service와 ftp-spring-boot-starter 추가
    
    * refactor: ftp-starter 이름 수정 및 위치 이동
    
    * refactor: ftp-starter 패키지명 변경
    
    - 기존 com.hermes.ftpspringbootstarter -> com.hermes.ftp-starter로 변경
    - 스타터 내부 클래스, 패키지 임포트 경로 모두 수정
    - 멀티모듈 구조에서 일관성 있는 패키지명 적용
    
    * refactor: ApiResponse를 공통 라이브러리로 변경 및 파일 URL 반환 메서드 오류 수정
    - 파일 이름을 반환하던 메서드를 다운로드 가능한 url 반환하게 수정
    
    * refactor: 불필요한 프로젝트 경로 지정 제거

---
+++ communication-service/src/main/java/com/hermes/communicationservice/FtpController.java
@@ -0,0 +1,49 @@
+package com.hermes.communicationservice;
+
+
+import com.hermes.api.common.ApiResponse;
+
+import com.hermes.ftpstarter.dto.FtpResponseDto;
+import com.hermes.ftpstarter.service.FtpService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+@RestController
+@RequestMapping("/ftp")
+@RequiredArgsConstructor
+@ConditionalOnProperty(prefix = "ftp", name = "enabled", havingValue = "true")
+public class FtpController {
+
+  @Autowired
+  private final FtpService ftpService;
+
+  // 파일 업로드
+  @PostMapping("/upload")
+  public ApiResponse<FtpResponseDto> upload(@RequestParam("file") MultipartFile file) {
+    FtpResponseDto response = ftpService.uploadFile(file);
+    return ApiResponse.success("업로드 성공", response);
+  }
+
+  // 파일 삭제
+  @DeleteMapping("/delete")
+  public ApiResponse<Object> delete(@RequestParam("filename") String filename) {
+    ftpService.deleteFile(filename);
+    return ApiResponse.success("삭제 성공");
+  }
+
+  // 다운로드 주소 반환
+  @GetMapping("/file-url")
+  public ApiResponse<String> getFileUrl(@RequestParam String fileName) {
+    String url = ftpService.getFileUrl(fileName);
+    return ApiResponse.success("다운로드 주소 반환 성공", url);
+  }
+
+}
