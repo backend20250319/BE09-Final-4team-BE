7e8b7714119fc93612ccf49d778d0472a0b0e706
hyeblee <hyeb1224@naver.com>
2025-08-31 02:43:21 +0900

    refactor: controller에 /api/communication 추가

--- communication-service/src/main/java/com/hermes/communicationservice/announcement/controller/AnnouncementController.java
+++ communication-service/src/main/java/com/hermes/communicationservice/announcement/controller/AnnouncementController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/announcements")
+@RequestMapping("/api/communication/announcements")
 @RequiredArgsConstructor
 @Slf4j
 public class AnnouncementController {
