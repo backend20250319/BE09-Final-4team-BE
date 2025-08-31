2dc20ee4729601484cd297babc86f0bd406a4136
hyeblee <hyeb1224@naver.com>
2025-08-31 18:22:35 +0900

    refactor: 댓글 API 경로에서 communication제거 및 게이트웨이 라우팅 수정

--- communication-service/src/main/java/com/hermes/communicationservice/comment/controller/CommentController.java
+++ communication-service/src/main/java/com/hermes/communicationservice/comment/controller/CommentController.java
@@ -15,7 +15,7 @@ import java.util.List;
 @Slf4j
 @RestController
 @RequiredArgsConstructor
-@RequestMapping("api/communication/")
+@RequestMapping("api/")
 public class CommentController {
 
     private final CommentService commentService;
