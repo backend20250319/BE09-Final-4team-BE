d838a253339bc80191c2c9137134555a679778b9
hyeblee <hyeb1224@naver.com>
2025-08-31 18:17:32 +0900

    refactor: AttachmentInfoResponse의 downloadUrl를 상대경로로 수정
    - baseUrl은 프론트에서 붙임

--- attachment-service/src/main/java/com/hermes/attachmentservice/controller/AttachmentController.java
+++ attachment-service/src/main/java/com/hermes/attachmentservice/controller/AttachmentController.java
@@ -110,8 +110,7 @@ public class AttachmentController {
             response.setDisplayFileName(metadata.getOriginalFileName());
             response.setFileSize(metadata.getFileSize());
             response.setContentType(metadata.getContentType());
-            response.setDownloadUrl("http://gateway-server:8080/attachment-service/api/attachments/" + fileId + "/download");
-            
+            response.setDownloadUrl("/api/attachments/" + fileId + "/download");
             return ResponseEntity.ok(ApiResult.success(response));
             
         } catch (Exception e) {
