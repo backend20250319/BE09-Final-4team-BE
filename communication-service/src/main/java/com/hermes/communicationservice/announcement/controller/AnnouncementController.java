ec57c96a9d299d5cb3121a89a0f63df5ad4d02dd
hyeblee <hyeb1224@naver.com>
2025-08-31 18:20:09 +0900

    refactor: 공지사항 api주소에서 communication 제거

--- communication-service/src/main/java/com/hermes/communicationservice/announcement/controller/AnnouncementController.java
+++ communication-service/src/main/java/com/hermes/communicationservice/announcement/controller/AnnouncementController.java
@@ -14,7 +14,7 @@ import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/communication/announcements")
+@RequestMapping("/api/announcements")
 @RequiredArgsConstructor
 @Slf4j
 public class AnnouncementController {
