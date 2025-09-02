768a9ced4ee0dd7f604b2cb6f7a0cf386bb84c5a
hyeblee <hyeb1224@naver.com>
2025-09-02 22:13:38 +0900

    feat: AMQP 리소스를 자동으로 생성하는 rabbitAdmin 추가

--- libs/notification-starter/src/main/java/com/hermes/notification/config/NotificationAutoConfiguration.java
+++ libs/notification-starter/src/main/java/com/hermes/notification/config/NotificationAutoConfiguration.java
@@ -11,6 +11,7 @@ import org.springframework.amqp.core.TopicExchange;
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
 import org.springframework.amqp.rabbit.config.SimpleRabbitListenerContainerFactory;
 import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.beans.factory.annotation.Value;
@@ -84,6 +85,12 @@ public class NotificationAutoConfiguration {
   @ConditionalOnMissingBean
   public NotificationPublisher notificationPublisher(NotificationSender notificationSender) {
     return new NotificationPublisher(notificationSender);
+  }
+
+  @Bean
+  @ConditionalOnMissingBean
+  public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory) {
+    return new RabbitAdmin(connectionFactory);
   }
 
 }
