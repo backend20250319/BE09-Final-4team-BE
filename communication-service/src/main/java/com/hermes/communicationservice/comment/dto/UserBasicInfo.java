6868ce9334a34366f03eb81819d81a7eba0660cb
hyeblee <hyeb1224@naver.com>
2025-09-03 10:28:41 +0900

    refactor: UserBasicInfo의 profileUrl -> profileImageUrl로 변경
    - MainProfileResponseDto와 필드명을 통일을 위함.

--- communication-service/src/main/java/com/hermes/communicationservice/comment/dto/UserBasicInfo.java
+++ communication-service/src/main/java/com/hermes/communicationservice/comment/dto/UserBasicInfo.java
@@ -12,5 +12,5 @@ import lombok.NoArgsConstructor;
 public class UserBasicInfo {
     private Long id;
     private String name;
-    private String profileUrl;
+    private String profileImageUrl;
 }
