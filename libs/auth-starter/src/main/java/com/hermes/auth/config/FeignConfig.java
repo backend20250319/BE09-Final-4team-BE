6ca8602090a461a5a7f95fa5775d74152be06de1
Seokjin Lee <seokjin.dev@gmail.com>
2025-09-04 12:25:21 +0900

    FeignConfig을 공용으로 만들기 위해 auth-starter로 이동 (#112)

--- libs/auth-starter/src/main/java/com/hermes/auth/config/FeignConfig.java
+++ libs/auth-starter/src/main/java/com/hermes/auth/config/FeignConfig.java
@@ -1,9 +1,8 @@
-package com.hermes.attendanceservice.config;
+package com.hermes.auth.config;
 
 import feign.RequestInterceptor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.context.request.RequestContextHolder;
 import org.springframework.web.context.request.ServletRequestAttributes;
 
@@ -11,7 +10,7 @@ import jakarta.servlet.http.HttpServletRequest;
 
 @Configuration
 public class FeignConfig {
-    
+
     @Bean
     public RequestInterceptor requestInterceptor() {
         return requestTemplate -> {
@@ -25,4 +24,4 @@ public class FeignConfig {
             }
         };
     }
-} 
+}
