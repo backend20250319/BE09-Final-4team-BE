452aca5efe485d21ac41fd6aa08bd077f2fe2dd6
Seokjin Lee <seokjin.dev@gmail.com>
2025-09-07 00:12:26 +0900

    AttachmentServiceClient 수정 (ApiResult 제거)

--- libs/attachment-client-starter/src/main/java/com/hermes/attachment/client/AttachmentServiceClient.java
+++ libs/attachment-client-starter/src/main/java/com/hermes/attachment/client/AttachmentServiceClient.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-@FeignClient(name = "attachment-service", fallback = AttachmentServiceClientFallback.class)
+@FeignClient(name = "attachment-service")
 public interface AttachmentServiceClient {
     
     @GetMapping("/api/attachments/{fileId}/info")
-    ApiResult<AttachmentInfoResponse> getFileMetadata(@PathVariable("fileId") String fileId);
+    AttachmentInfoResponse getFileMetadata(@PathVariable("fileId") String fileId);
 }
